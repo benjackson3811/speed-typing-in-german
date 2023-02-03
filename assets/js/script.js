109a5477ad202f533ee723553211fbf0346b9664
benjackson3811 <benjackson3811@googlemail.com>
2023-02-03 09:16:21 +0000

    Add german source words data file to JS folder

---
+++ assets/js/script.js
@@ -0,0 +1,30 @@
+
+
+
+/** The main game - calling the german word when the script is first loaded
+ * and after the user's answer has been processed
+ 
+ * possibly change the funciton name to word quote */
+function runGame () {
+    /** where to put the word quote function to be created */ 
+}
+
+function getRandomWord () {
+
+}
+
+function getNewWord () {
+
+}
+
+
+/*Focus on Input Field*/
+
+/* Scoring for each correct answer*/
+function incrementScore () {
+
+}
+/* Scoring for each incorrect answer*/
+function incrementIncorrectScore () {
+
+}
