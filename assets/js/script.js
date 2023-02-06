5a607108332f76030aedcbfa88d71292e9c00556
benjackson3811 <benjackson3811@googlemail.com>
2023-02-06 17:06:46 +0000

    fix for favicon error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,21 @@
-// click on start game button
-const startGameButton = document.getElementsByClassName('btn--start');
-const wordInput = document.getElementsByClassName('word-input');
+let startGameButton = document.getElementsByClassName('button')
 const splittedGermanWords = germanWords.split(" "); 
-const wordToType = document.getElementById('wordToType')
+const wordDisplayElement = document.getElementById('wordDisplay');
+const input = document.getElementById('wordInput');
 
 function getRandomWord(){
     return splittedGermanWords[ Math.floor(Math.random() * splittedGermanWords.length)];
 }
 
-function nextWord() {
-    const word = getRandomWord();
-    wordToType.textContent = word;
+function nextWord(){
+    const word =getRandomWord();
+    wordDisplayElement.textContent = word;    
 }
 
 console.log(getRandomWord());
 
 startGameButton.addEventListener('click', function(event){
     event.preventDefault();
-// show the input field
-
-// focus on the input field
-wordInput.focus();
-// show the word
-nextWord();
-// start the timer 
+    input.className ='';
+    nextWord();
 })
-
-
-// When user enters an input
-
-// check if correct
-
-// if correct, add the word to the list with green background
-// add 1 to score
-
-// if incorrect, add the word to the list with redground
-
-// clear user input
-
-// show the next word
-
-//Stats
-
-// Calc the stats
-//check if game has ended
-
-// When Game has ended, alert player to score
-// refresh user page
