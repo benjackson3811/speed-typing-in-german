eec341cf90661fa5f6a5a4a6ffedadef1d22d232
benjackson3811 <benjackson3811@googlemail.com>
2023-02-07 15:53:23 +0000

    Add JS timer function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,24 +2,49 @@ const startGameButton = document.getElementById('btn--start');
 const splittedGermanWords = germanWords.split(" "); 
 const wordDisplay = document.getElementById('wordDisplay');
 const input = document.getElementById('wordInput');
+const timer = document.getElementById('timer');
 
-/*storing data in html */
+/**
 const condition = {
-    currentWord:''
-}
+    currentWord:'',
+    score: 0,
+    timeElapsed:0,
+    GameLength:60,
+}**/
 
-input.addEventListener('input',()=>{
-    console.log(changed)
-})
 
 startGameButton.addEventListener('click',startGame());
     if (startGame.type === 'btn--start');
+    startGame();
+
+
+input.addEventListener('input',()=>{
+    const wordArray = wordDisplay.querySelectorAll('span');
+    const inputArray = input.value.split('');
+
+    let correct = true
+    wordArray.forEach((characterSpan,index) => {
+        const character = inputArray[index]
+        if (character == null) {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+        } else if (character === characterSpan.innerText){
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } else {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+            correct = false
+        }
+    })
+
+    if (correct) nextWord()
+})
     
-/*startGame Function */
-function startGame(){
-    nextWord();
-    input.focus();
-    input.value = null;
+    
+
+    
 }
 /*Function to create a random word from german-word.js array */
 function getRandomWord(){
@@ -27,23 +52,38 @@ function getRandomWord(){
     };
 
 /*Creating a new word for the DOM inner text */
+
 function nextWord(){
     let word = getRandomWord();
     word.split('').forEach(character =>{;
-        const characterSpan = document.createElement('span');
-        characterSpan.classList.add('correct')
-        characterSpan.innerText = character;
-        wordDisplay.appendChild(characterSpan);
+    const characterSpan = document.createElement('span');
+    characterSpan.innerText = character;
+    wordDisplay.appendChild(characterSpan);
     })
+    input.value = null;
+  
 }
 console.log(getRandomWord());
 
-
-
-
-
 /**Timer */
- /**Reset input */   
-/*check answer*/
+let startTime
+function timerStart() {
+    timer.innerText = 0
+    startTime = new Date()
+    setInterval(()=>{
+       timer.innerText = timerTime()
+    },1000
+    )
+}
 
-/*Calculate score */
+function timerTime() {
+    return Math.floor((new Date() - startTime)/1000)
+}
+
+
+/*startGame Function */
+function startGame(){
+    nextWord();
+    input.focus();
+    timerStart();
+    wordDisplay.value = null;
